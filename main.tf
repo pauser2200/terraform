fe757d870fe7e43dddc8b98e4959b2466b78d8e0
pauser2200 <pauser2200@gmail.com>
2025-03-11 19:29:15 -0300

    Ajustando ansible

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami           = "ami-00c257e12d6828491"
   instance_type = "t2.micro"
- key_name = "iac_alura1"
+ key_name = "iac_key"
   tags = {
     Name = "primeira_instancia"
   }
